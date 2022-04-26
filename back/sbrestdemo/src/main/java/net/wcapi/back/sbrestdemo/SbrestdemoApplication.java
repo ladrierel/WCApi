31fdf337498024ca10d95db1dad160f1255473d9
Laurent Ladriere <laurent.ladriere@cgi.com>
2022-04-26 17:19:50 +0200

    Generating SB RESTFul Web Service Demo

---
+++ back/sbrestdemo/src/main/java/net/wcapi/back/sbrestdemo/SbrestdemoApplication.java
@@ -0,0 +1,13 @@
+package net.wcapi.back.sbrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbrestdemoApplication.class, args);
+	}
+
+}
---
+++ back/sbrestdemo/src/test/java/net/wcapi/back/sbrestdemo/SbrestdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wcapi.back.sbrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrestdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
