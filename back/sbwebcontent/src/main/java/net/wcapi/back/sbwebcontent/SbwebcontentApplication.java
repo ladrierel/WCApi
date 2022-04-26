b73ae69ca34b425ab6f9beca624b6232e515d5eb
Laurent Ladriere <laurent.ladriere@cgi.com>
2022-04-26 10:47:53 +0200

    Generating SB WebContent Demo

---
+++ back/sbwebcontent/src/main/java/net/wcapi/back/sbwebcontent/SbwebcontentApplication.java
@@ -0,0 +1,13 @@
+package net.wcapi.back.sbwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwebcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbwebcontentApplication.class, args);
+	}
+
+}
---
+++ back/sbwebcontent/src/test/java/net/wcapi/back/sbwebcontent/SbwebcontentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wcapi.back.sbwebcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwebcontentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
