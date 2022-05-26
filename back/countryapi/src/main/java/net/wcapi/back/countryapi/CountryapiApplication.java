1f2e1c3bd52a8c78afbee753e9c5f1c26e7d10a8
Laurent Ladriere <laurent.ladriere@cgi.com>
2022-05-26 16:55:23 +0200

    Add logger

--- back/countryapi/src/main/java/net/wcapi/back/countryapi/CountryapiApplication.java
+++ back/countryapi/src/main/java/net/wcapi/back/countryapi/CountryapiApplication.java
@@ -8,8 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.File;
 import java.io.IOException;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 @SpringBootApplication
 public class CountryapiApplication implements CommandLineRunner {
+
+    //create Logger
+    private static final Logger LOGGER = Logger.getLogger(CountryapiApplication.class.getPackage().getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryapiApplication.class, args);
@@ -24,10 +30,15 @@ public class CountryapiApplication implements CommandLineRunner {
         //read json file and convert to customer object
         Rankings rank = objectMapper.readValue(new File("ranking.json"), Rankings.class);
 
-        //print rank details
-		System.out.println(rank);
-        System.out.println(rank.getRankings().get(0));
-        System.out.println(rank.getRankings().get(1));
+        //print rank details as Debug
+        LOGGER.log(Level.INFO, "Get the Mapping for JSON file ranking.json");
+        LOGGER.log(Level.INFO, "rank" + rank.toString());
+        LOGGER.log(Level.INFO, "rank index 0" + rank.getRankings().get(0).toString());
+        LOGGER.log(Level.INFO, "rank index 1" + rank.getRankings().get(1).toString());
+
+		//System.out.println(rank);
+        //System.out.println(rank.getRankings().get(0));
+        //System.out.println(rank.getRankings().get(1));
 		// System.out.println(rank[1]);
     }
 
